39feace6ce399e85a4a19392bd77b2e3e2278902
aderdzinski <aderdzinski@gmail.com>
2016-07-12 14:54:49 -0400

    CI looks right, but needs commenting

--- andrea/confidence_andrea.py
+++ andrea/confidence_andrea.py
@@ -3,6 +3,7 @@ import pylab as plt
 import numpy as np
 from matplotlib import rc
 from scipy.optimize import curve_fit
+import scipy.stats as stats
 # Some plotting preferences
 rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
 rc('text', usetex=True)
@@ -12,86 +13,109 @@ plt.rcParams['lines.linewidth'] = 2
 
 # Read in the data by each column
 year,jan,feb,mar,apr,may,jun,jul,aug,sep,oco,nov,dec,JmD,DmN,DJF,MAM,JJA,SON,yr\
-=np.genfromtxt('GIS_plots/GIST.txt',unpack=True,skip_footer=1)
-a = np.where(MAM < 900)
+=np.genfromtxt('../GIS_plots/GIST.txt',unpack=True,skip_footer=1)
+#a = np.where(MAM < 900)
 
 
 # Define a 'straight line' y=f(x)
-def f(x, A, B): 
-    return A*x + B
+#def f(x, A, B): 
+#    return A*x + B
 
 # Fit your data to a line
 # This returns the slope and y-intercept of the fit
-A,B = curve_fit(f, year[a], JmD[a])[0] 
+#A,B = curve_fit(f, year, JmD)[0] 
 
 
-# ---------------------------------
-# Everything below is taken 
-# from http://www.sjsu.edu/faculty/gerstman/StatPrimer/regression.pdf
-# Calculate the standard error of the regression
-# First the sum of squares of X and Y values:
-SSyy = np.sum((JmD - np.mean(JmD))**2)
-SSxx = np.sum((year - np.mean(year))**2)
-SSxy = np.sum((year - np.mean(year))*(JmD - np.mean(JmD)))
 # Sample size
-n = len(year[a])
-# Standard error of the regression:
-se_reg = (SSyy - B*SSxy/(n-2))**0.5
-# Standard error of the slope:
-#se = se_reg / SSxx**0.5
-# 95% confidence interval
-# CI = Z * sigma/sqrt(n)
-# Look up Z from a table for 95% 
-#CI = 1.96*se/n**0.5
-# Something wrong with this, cause CI is way too small
-# ----------------------------
-
-# Not sure about above, let's try something simpler
-# Compute the standard deviation of the residuals
-se = (np.sum((JmD - np.mean(JmD))**2)/n)**0.5
-CI = 1.96*se/n**0.5
-t_value = 2. # Look this up
-# This gives a stupidly small CI too?
-#CI = t_value*se*np.sqrt(1/n + (year -np.mean(year))**2/(n-1)/SSxx**2)
+n = len(year)
 
 
-#------------------------------
-# Ok, let's try these equations again
-# A confidence interval is given by the 
-# sum of the squares of the standard error of the mean
-# and the standard error of the slope
+# COMMENT ALL THIS
 
-# First I should shift my data so that 
-# it is centered about the mean: 
+x = year
+y = JmD
 
-# Then I can compute the standard deviation:
+Sxx = np.sum(x**2) - np.sum(x)**2/n
+Sxy = np.sum(x*y) - np.sum(x)*np.sum(y)/n
 
-# and get the standard error of the mean 
-# by dividing by the sqrt of the sample size:
+# Linefit
+b = Sxy/Sxx
+a = np.mean(y) - b*np.mean(x)
 
-# Then I can get the standard error of the 
-# slope of the shifted line at the mean:
+# Residuals
+fit = lambda xx: a + b*xx    
+residuals = y - fit(x)
 
-# Now the error on the slope at some *other* value of x
-# Is the error on the slope times some distance from the mean:
+var_res = np.sum(residuals**2)/(n-2)
+sd_res = np.sqrt(var_res)
 
-# Now we can get the standard error of 
-# an estimate of the mean at some other value of x:
+# Confidence intervals
+se_b = sd_res/np.sqrt(Sxx)
+se_a = sd_res*np.sqrt(np.sum(x**2)/(n*Sxx))
 
-# If you plot this as a function of x, 
-# It should look like a curve:
+
+df = n-2                            # degrees of freedom
+alpha=0.05
+tval = stats.t.isf(alpha/2., df) 	# appropriate t value
+
+ci_a = a + tval*se_a*np.array([-1,1])
+ci_b = b + tval*se_b*np.array([-1,1])
 
 
 
 
+# create series of new test x-values to predict for
+npts = 100
+px = np.linspace(np.min(x),np.max(x),num=npts)
+    
+se_fit     = lambda x: sd_res * np.sqrt(  1./n + (x-np.mean(x))**2/Sxx)
+se_predict = lambda x: sd_res * np.sqrt(1+1./n + (x-np.mean(x))**2/Sxx)
 
 
+# Return info
+ri = {'residuals': residuals, 
+    'var_res': var_res,
+    'sd_res': sd_res,
+    'alpha': alpha,
+    'tval': tval,
+    'df': df}
 
+# Plot the data
+plt.figure()
+x.sort()
+limit = (1-alpha)*100
+plt.plot(x, fit(x)+tval*se_fit(x), 'r', label=r'Confidence limit ({0:.1f}\%)'.format(limit))
+plt.plot(x, fit(x)-tval*se_fit(x), 'r')
+plt.fill_between(x,fit(x)+tval*se_fit(x),fit(x)-tval*se_fit(x),color='red',alpha=0.2)
+
+
+plt.plot(px, fit(px),'black', label='Regression line')
+plt.plot(x,y, label='Sample observations',linestyle='none',marker='o',color='black',markersize=3)
+
+
+        
+plt.plot(x, fit(x)+tval*se_predict(x), 'c--', label=r'Prediction limit ({0:.1f}\%)'.format(limit))
+plt.plot(x, fit(x)-tval*se_predict(x), 'c--')
+
+plt.ylabel('(milli?) degrees')
+plt.xlabel('year')
+plt.title('Linear regression and confidence limits')
+        
+# configure legend
+plt.legend(loc=0)
+leg = plt.gca().get_legend()
+ltext = leg.get_texts()
+plt.setp(ltext, fontsize=10)
+
+plt.show()
+
+
+'''
 # Plot it
 plt.fill_between(year,f(year,A,B)+CI,f(year,A,B)-CI,color='#8258FA',alpha=0.5)
 plt.plot(year,f(year,A,B)+CI,color='#8258FA',linewidth=2,label=r'$\rm CI$')
 plt.plot(year,f(year,A,B)-CI,color='#8258FA',linewidth=2)
-plt.plot(year[a],JmD[a],color='#0431B4',linewidth=2,label=r'$\rm annual \, mean$')
+plt.plot(year,JmD,color='#0431B4',linewidth=2,label=r'$\rm annual \, mean$')
 plt.plot(year,f(year,A,B),color='black',linewidth=2,label=r'$\rm fit$')
 plt.plot(year,f(year,A,B)+se,color='grey',linestyle='--',linewidth=2,label=r'$\sigma$')
 plt.plot(year,f(year,A,B)-se,color='grey',linewidth=2,linestyle='--')
@@ -99,3 +123,4 @@ plt.legend(loc=2)
 plt.xlabel(r'$\rm year$')
 plt.ylabel(r'$\rm 0.01 C/decade$')
 plt.show()
+'''
