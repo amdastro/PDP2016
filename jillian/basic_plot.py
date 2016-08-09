8277d8aa5f487814fb32080a3eec30326689ae5a
Jillian <jbellovary@amnh.org>
2016-08-09 11:19:32 -0400

    added comments

--- jillian/basic_plot.py
+++ jillian/basic_plot.py
@@ -1,25 +1,37 @@
+#### the format of the original datafile columns are like so:  ###
+
+#[,0] 	"alcgp" 	Alcohol consumption
+#                       1 0--39 gm/day
+#			2 40--79
+#			3 80--119
+#			4 120+
+#[,1] 	"tobgp" 	Tobacco consumption
+#                       1 0-- 9 gm/day
+#			2 10--19
+#			3 20--29
+#			4 30+
+#[,2] 	"ncases" 	Number of cases 	
+###################################################
+
+# import some libraries that we will need
 import numpy as np
 import matplotlib.pyplot as plt
 
+##  read in the data file  ##
+##  columns separated by commas.  We don't want the first row.
 data = np.genfromtxt("noage_esoph.csv",delimiter=",",skip_header=1,dtype='str')
 
-alc=[]     #  grams per day
-tob=[]     #  grams per day
-ncases=[]
-
-for row in data:
-    alc.append(float(row[0]))
-    tob.append(float(row[1]))
-    ncases.append(float(row[2]))
+# the result "data" is a matrix of all of the information.
+# Now we need to organize it a little bit.
+alc = data[:,0]
+tob = data[:,1]
+ncases = data[:,2]
 
 
+# turn them into numpy arrays so we can do math with them
 alc = np.array(alc)
 tob = np.array(tob)
 ncases = np.array(ncases)
-
-### find unique values of the arrays
-unique_alc = np.unique(alc)
-unique_tob = np.unique(tob)
 
 ### plot the things.
 plt.scatter(tob,ncases)
--- jillian/fit_a_line.py
+++ jillian/fit_a_line.py
@@ -3,23 +3,13 @@ import matplotlib.pyplot as plt
 
 data = np.genfromtxt("noage_esoph.csv",delimiter=",",skip_header=1,dtype='str')
 
-alc=[]     #  grams per day
-tob=[]     #  grams per day
-ncases=[]
-
-for row in data:
-    alc.append(float(row[0]))
-    tob.append(float(row[1]))
-    ncases.append(float(row[2]))
-
+alc = data[:,0]
+tob = data[:,1]
+ncases = data[:,2]
 
 alc = np.array(alc)
 tob = np.array(tob)
 ncases = np.array(ncases)
-
-### find unique values of the arrays
-unique_alc = np.unique(alc)
-unique_tob = np.unique(tob)
 
 ### fit the line using polyfit  ###
 fit = np.polyfit(tob,ncases,1)
